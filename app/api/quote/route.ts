af65db3415eddabdf789b9c29ed5a17598b49e03
rajeshwormoirangthem-hue <rajeshwormoirangthem@gmail.com>
2026-01-14 11:48:55 +0530

    R

--- app/api/quote/route.ts
+++ app/api/quote/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    quote: "HELLOW RAJESDHWOR MOIRANGTHEMS!",
+    quote: "HELLOW RAJESDHWOR MOIRANGTHEM!",
   });
 }
